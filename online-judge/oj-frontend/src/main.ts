a8cadcd13122fbdaff96d55f86e3a8c04a295d9a
Jialong-c <57862215+Jialong-c@users.noreply.github.com>
2023-09-14 14:09:37 +0800

    知识星球OJ系统前后端初始化，前端登录、注册页面开发

---
+++ online-judge/oj-frontend/src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import ArcoVue from "@arco-design/web-vue";
+import "@arco-design/web-vue/dist/arco.css";
+import router from "./router";
+import store from "./store";
+import "@/plugins/axios";
+import "@/access";
+import "bytemd/dist/index.css";
+
+// 额外引入图标库
+import ArcoVueIcon from "@arco-design/web-vue/es/icon";
+
+createApp(App)
+  .use(ArcoVue)
+  .use(store)
+  .use(router)
+  .use(ArcoVueIcon)
+  .mount("#app");
