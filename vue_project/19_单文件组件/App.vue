712b642d86812eb5ca2c8e64334542646825c4e7
Jialong-c <57862215+Jialong-c@users.noreply.github.com>
2023-10-18 13:33:57 +0800

    Vue非单文件组件，单文件组件

---
+++ vue_project/19_单文件组件/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <School/>
+        <Student/>
+    </div>
+</template>
+
+<script>
+//引入组件
+import School from './School.Vue'
+import Student from './Student.Vue'
+
+export default {
+    name: 'App',
+    components: {
+        School,
+        Student
+    }
+}
+</script>
