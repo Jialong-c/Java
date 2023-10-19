f95d51ebb237b69d7b528d74f09e1f51456b9f8c
Jialong-c <57862215+Jialong-c@users.noreply.github.com>
2023-10-20 00:05:53 +0800

    Vue-CLI分析脚手架，ref属性，props配置，mixin混入，插件，scoped样式

---
+++ vue-cli-test/vue.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  pages:{
+    index:{
+      //入口
+      entry:'src/main.js'
+    }
+  },
+  //关闭语法检查
+  lintOnSave:false
+})
