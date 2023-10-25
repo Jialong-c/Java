98b8fe72dbc10b96654f2f240e8a96d789ad9a2b
Jialong-c <57862215+Jialong-c@users.noreply.github.com>
2023-10-25 22:57:31 +0800

    Vue-CLI 过渡与动画，TodoList动画，Vue ajax配置代理服务器

---
+++ vue-cli-test/19_src_配置代理服务器/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <button @click="getStudents">获取学生信息</button>
+    <button @click="getCars">获取汽车信息</button>
+  </div>
+</template>
+
+<!--跨域是浏览器行为，不是服务器行为-->
+<script>
+  import axios from "axios";
+  export default {
+    name:'App',
+    methods:{
+      getStudents(){
+        axios.get('http://localhost:8080/api/students').then(
+            response =>{
+              console.log('请求成功',response.data)
+            },
+            error =>{
+              console.log('请求失败',error.message)
+            }
+        )
+      },
+      getCars(){
+        axios.get('http://localhost:8080/demo/cars').then(
+            response =>{
+              console.log('请求成功',response.data)
+            },
+            error =>{
+              console.log('请求失败',error.message)
+            }
+        )
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- vue-cli-test/vue.config.js
+++ vue-cli-test/vue.config.js
@@ -8,5 +8,28 @@ module.exports = defineConfig({
     }
   },
   //关闭语法检查
-  lintOnSave:false
+  lintOnSave:false,
+  //开启代理服务器（方式一）
+  /*devServer:{
+      proxy: 'http://localhost:5000', //将请求转发给端口号5000，注意这里只能配置单个代理，不能配置多个代理
+  }*/
+  //开启代理服务器（方式一）
+  devServer:{
+    proxy:{
+      //请求前缀
+      '/api':{
+        target:'http://localhost:5000',
+        //正则表达式匹配请求前缀，并替换成空字符串
+        pathRewrite:{'^/api':''},
+        ws: true, //用于支持websocket
+        changeOrigin: true, //用于控制请求头中host的值,false表示5000端口服务器询问代理服务器端口时代理服务器如实回答，否则回答5000端口
+      },
+      'demo':{
+        target: 'http://localhost:5001',
+        pathRewrite: {'^/demo':''},
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
 })
